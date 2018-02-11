9fdc624cebfe6de7b34653466e51c60b5f9c8778
Brian Fitzgerald <BrianFitzgerald@carfax.com>
2018-02-11 17:54:53 -0600

    linting and story update

--- src/pages/RoomSetupView.tsx
+++ src/pages/RoomSetupView.tsx
@@ -1,73 +1,74 @@
-import * as React from 'react';
+import * as React from "react"
 import {
-    Platform,
-    StyleSheet,
-    Text,
-    ScrollView,
-    StatusBar,
-    View,
-    ScrollViewProps,
-    ScrollViewStatic
-} from 'react-native';
-import commonStyles from '../styles/commonStyles';
-import HeroButton from '../components/HeroButton';
-import colors from '../styles/colors';
+  Platform,
+  StyleSheet,
+  Text,
+  ScrollView,
+  StatusBar,
+  View,
+  ScrollViewProps,
+  ScrollViewStatic
+} from "react-native"
+import commonStyles from "../styles/commonStyles"
+import HeroButton from "../components/HeroButton"
+import colors from "../styles/colors"
 
-import { dbInstance } from '../firebaseRef';
-import { Story, StoryOption } from '../types/Story';
-import { Player } from '../types/Player';
-import { getNextActionIndex, doAction, getActionByIndex } from '../actions/Story';
-import { RoomState, FirebaseRoomState } from '../types/Network';
-import { roomDefaultState, updateRoomState } from '../firebaseFunctions';
+import { dbInstance } from "../firebaseRef"
+import { Story, StoryOption } from "../types/Story"
+import { Player } from "../types/Player"
+import {
+  getNextActionIndex,
+  doAction,
+  getActionByIndex
+} from "../actions/Story"
+import { RoomState, FirebaseRoomState } from "../types/Network"
+import { roomDefaultState, updateRoomState } from "../firebaseFunctions"
 
 type RoomSetupViewProps = {
-    story: Story
-    currentPlayerName: string
-    roomCode: string
-    dispatch?: (func: ({ type: string; value: RoomState; })) => void
+  story: Story
+  currentPlayerName: string
+  roomCode: string
+  dispatch?: (func: { type: string; value: RoomState }) => void
 }
 
 type RoomSetupViewState = {
-    selectedStoryID: number
+  selectedStoryID: number
 }
 
-export default class PartyView extends React.Component<RoomSetupViewProps, RoomSetupViewState> {
-
-    render() {
-
-        return (
-            <View style={[commonStyles.container, styles.partyContainer]}>
-                <StatusBar
-                    backgroundColor={colors.black}
-                    barStyle="light-content"
-                />
-                
-            </View>
-        )
-    }
+export default class RoomSetupView extends React.Component<
+  RoomSetupViewProps,
+  RoomSetupViewState
+> {
+  render() {
+    return (
+      <View style={[commonStyles.container, styles.partyContainer]}>
+        <StatusBar backgroundColor={colors.black} barStyle="light-content" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    promptText: {
-        fontSize: 24,
-        color: colors.grey
-    },
-    currentPromptText: {
-        fontSize: 24,
-        color: colors.white,
-        textAlign: 'left'
-    },
-    partyContainer: {
-        flex: 1,
-        flexDirection: 'column'
-    },
-    playersWhoSelectedOption: {
-        fontSize: 18,
-        color: 'white'
-    },
-    promptButton: {
-        width: '100%',
-        marginBottom: 12,
-        marginTop: 4
-    }
-});
+  promptText: {
+    fontSize: 24,
+    color: colors.grey
+  },
+  currentPromptText: {
+    fontSize: 24,
+    color: colors.white,
+    textAlign: "left"
+  },
+  partyContainer: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  playersWhoSelectedOption: {
+    fontSize: 18,
+    color: "white"
+  },
+  promptButton: {
+    width: "100%",
+    marginBottom: 12,
+    marginTop: 4
+  }
+})
