842c7fe8fb2213b3dd380086085bbcc75b35f5cf
Brian Fitzgerald <BrianFitzgerald@carfax.com>
2018-02-11 19:03:54 -0600

    rm secrets

--- src/secrets.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigurationOptions } from "aws-sdk/lib/config"
-
-export const awsKeys: ConfigurationOptions = {
-  region: "us-east-1",
-  accessKeyId: "AKIAJ5Z7YEII7PO5ADDQ",
-  secretAccessKey: "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"
-}
-
-export const firebaseKey = "AIzaSyAE53GY_t5lKckauLUUmM4zOOBhuc7qgcg"
